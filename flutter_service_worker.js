2080f40613c51e9ebd3824e3faacf7997a68f9d5
demoVortex <81253554+demoVortex@users.noreply.github.com>
2021-12-22 16:25:51 +0400

    sdf

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "51a6b5edd5a7e6ae2b9f9772af46ad62",
-"index.html": "1cfdc751ec3a1bc5c90653d272fb31f5",
-"/": "1cfdc751ec3a1bc5c90653d272fb31f5",
-"main.dart.js": "dff97f17ee9b7551004900602ace1ece",
+"index.html": "64e236580910a95a53e5627be8df2058",
+"/": "64e236580910a95a53e5627be8df2058",
+"main.dart.js": "2d786671fc4f31fdac5ab48f38469110",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
